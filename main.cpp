be919b1448680aed43c71aec47fa192b153d3f7a
alamahul <118677015+alamahul@users.noreply.github.com>
2025-01-16 00:09:31 +0700

    Mengubah nilai pencahayaan

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
 Ramdhani = 84 baris
 Agna = 81 baris
-Alamahul = 98 baris
+Alamahul = 98 baris 
 */
 //-------------------------------------------------------
 // Ramdhani Sulaeman Burhanudin
@@ -45,6 +45,7 @@ void drawText(float x, float y, float z, string text, int fontSize)
 }
 
 void balok(float merah = 0.0, float hijau = 0.0, float biru = 0.0, float kilauan = 0.0) {
+	
     glPushMatrix();
     // Atur properti material objek
     float mat_ambient[] = {merah*0.2, hijau*0.2, biru*0.2, 1.0f}; // Warna ambient
@@ -72,18 +73,18 @@ void light(){
     
     // Atur posisi sumber cahaya
     GLfloat light_position[] = {0.0f, 7.0f, 0.0f, 1.0f}; // Posisi sumber cahaya
-    GLfloat light_ambient[] = {1.0f, 1.0f, 1.0f, 1.0f}; // Warna ambient
-    GLfloat light_diffuse[] = {0.7f, 0.7f, 0.7f, 1.0f}; // Warna diffuse
+    GLfloat light_ambient[] = {0.7f, 0.7f, 0.7f, 1.0f}; // Warna ambient
+    GLfloat light_diffuse[] = {0.9f, 0.9f, 0.9f, 1.0f}; // Warna diffuse
     GLfloat light_specular[] = {1.0f, 1.0f, 1.0f, 1.0f}; // Warna specular
     GLfloat light_off[] = {0.0f, 0.0f, 0.0f, 0.0f}; // lampu mati
+    
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0,  GL_SPECULAR, light_specular);
  
-    if (lampu == true){
-    	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    if (lampu){
 		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-		glLightfv(GL_LIGHT0,  GL_SPECULAR, light_specular);	
 	}else{
-    	glLightfv(GL_LIGHT0, GL_AMBIENT, light_off);
 		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_off);
 		glLightfv(GL_LIGHT0,  GL_SPECULAR, light_off);		
 	} 
@@ -99,6 +100,7 @@ void drawCartecius() {
     drawText(0.5, -13.0, 0.0, "-Y", 60);
     drawText(0.0, 0.5, 23.0, "Z", 60);
     drawText(0.0, 0.5, -13.0, "-Z", 60);
+    
     // Gambar garis untuk sumbu
     glBegin(GL_LINES);
     // x line
@@ -116,13 +118,15 @@ void drawCartecius() {
 
 void pintu (){
 	glPushMatrix();
-	glTranslatef(-6.5, 3.0, -4.0);
+	glTranslatef(-6.5, 3.0, -4.0);		
+	
 	glScalef(0.2, 7.0, 2.0);
 	balok(0.3f, 0.3f, 0.3f);
 	glPopMatrix();
 	
 	glPushMatrix();
 	glTranslatef(-6.4, 3.0, -3.5);
+
 	glScalef(0.1, 0.8, 0.1);
 	balok(0.8f, 0.8f, 0.8f);
 	glPopMatrix();
@@ -198,7 +202,7 @@ void AC (){
 	glPushMatrix();
 	glTranslatef(-6.0, 8.0, 0.0);
 	glScalef(1.0, 2.0, 4.0);
-	balok(0.92f, 0.92f, 0.92f, 0.0f);
+	balok(0.92f, 0.92f, 0.92f);
 	glPopMatrix();
 }
 
@@ -224,12 +228,13 @@ void display(void)
 	atap();
 	pintu();
 	bor ();
-	AC();  
+	AC(); 
     glutSwapBuffers();
 }
 
 void myKeyboardFunc( unsigned char key, int x, int y )
 {
+	
     switch ( key )
     {
     case 'q':
@@ -277,7 +282,7 @@ void myKeyboardFunc( unsigned char key, int x, int y )
 		}
 		break;
 	}
-    glutPostRedisplay();
+   glutPostRedisplay();
 }
 //----------------------------------------------------end
 
@@ -294,7 +299,7 @@ int main (int argc, char **argv)
     glutCreateWindow("Ruangan Kelas TB");
     glShadeModel( GL_SMOOTH );
     glEnable( GL_DEPTH_TEST );
-    glEnable(GL_NORMALIZE);
+    glEnable( GL_NORMALIZE );
     glutKeyboardFunc(myKeyboardFunc);
     glutDisplayFunc(display);
     glutMainLoop();
